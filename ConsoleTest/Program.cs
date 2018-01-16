1961bdf3bd8aa9da5206fdb0b7a39e907d2b84e4
chenming <18280246919@163.com>
2018-01-16 23:32:06 +0800

    Console Test

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cfg = new Configuration().Configure("Config/hibernate.cfg.xml");
+            var session = cfg.BuildSessionFactory();
+            NHibernate.Tool.hbm2ddl.SchemaExport schemaExport = new NHibernate.Tool.hbm2ddl.SchemaExport(cfg);
+            //schemaExport.SetOutputFile(@"");
+            schemaExport.Drop(true, true);
+            schemaExport.Create(true, true);
+        }
+    }
+}
